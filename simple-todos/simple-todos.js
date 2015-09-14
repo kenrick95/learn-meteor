6ad052607d135ff999ea5d8cdd6fc6d33eddcb33
kenrick95 <kenrick95@gmail.com>
2015-09-14 11:09:14 +0800

    till 4. Forms and events

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,15 +1,48 @@
+Tasks = new Mongo.Collection("tasks");
+// Done until here https://www.meteor.com/tutorials/blaze/forms-and-events
 if (Meteor.isClient) {
   Template.body.helpers({
 
-    tasks: [
+    tasks: function () {
 
-      { text: "This is task 1" },
+      return Tasks.find({}, {sort: {createdAt: -1}});
 
-      { text: "This is task 2" },
+    }
 
-      { text: "This is task 3" }
+  });
+  Template.body.events({
 
-    ]
+    "submit .new-task": function (event) {
+
+      // Prevent default browser form submit
+
+      event.preventDefault();
+
+ 
+
+      // Get value from form element
+
+      var text = event.target.text.value;
+
+ 
+
+      // Insert a task into the collection
+
+      Tasks.insert({
+
+        text: text,
+
+        createdAt: new Date() // current time
+
+      });
+
+ 
+
+      // Clear form
+
+      event.target.text.value = "";
+
+    }
 
   });
 }
